cac1d9996b90eacf03778c0b5b1bdf9ef16534ea
dhavalhirdhav <dhaval_faria@hotmail.com>
2020-05-17 17:26:14 +0530

    basic vga improvements.

--- drivers/screen.c
+++ drivers/screen.c
@@ -1,10 +1,15 @@
 #include "screen.h"
 #include "ports.h"
+#include "../utils/utils.h"
 
 int CURRENT_CURSOR_POSITION = 0;
 
 void set_cursor_position(int offset);
 int get_cursor_position();
+
+int get_current_cursor_row();
+int get_current_cursor_column();
+int get_offset(int col, int row);
 
 void clear() {
     char* video_memory = (char*) VIDEO_ADDRESS;
@@ -12,21 +17,58 @@ void clear() {
     for(row = 0; row < TOTAL_COLS * TOTAL_ROWS; row++)
     {
         video_memory[row * 2] = ' ';
-        video_memory[row * 2 + 1] = 0x0f;
+        video_memory[row * 2 + 1] = STANDARD_MSG_COLOR;
     }
     CURRENT_CURSOR_POSITION = 0x00;
     set_cursor_position(CURRENT_CURSOR_POSITION);
 }
 
-void printf(char *str) {
+void printf(char *str, ...) {
     char* video_memory = (char*) VIDEO_ADDRESS;
     int pos = 0;
     int cursorPosition = get_cursor_position();
     while(str[pos] != 0)
     {
-        video_memory[cursorPosition] = str[pos++];
-        video_memory[cursorPosition + 1] = 0x0f;
-        cursorPosition = cursorPosition + 2;
+        int row = get_current_cursor_row(cursorPosition);
+        int column = get_current_cursor_column(cursorPosition);
+
+        if(str[pos] == '\n')
+        {
+            cursorPosition = get_offset(column, row + 1);
+            pos++;
+        }
+        else if(str[pos] == '\r')
+        {
+            cursorPosition = get_offset(0, row);
+            pos++;
+        }
+        else
+        {
+            video_memory[cursorPosition] = str[pos++];
+            video_memory[cursorPosition + 1] = STANDARD_MSG_COLOR;
+            cursorPosition = cursorPosition + 2;
+        }
+
+
+        //check are we on last row?
+        if(cursorPosition >= TOTAL_ROWS * TOTAL_COLS * 2)
+        {
+            int i;
+            /*shift the content by 1 line up*/
+            for(i = 1; i < TOTAL_ROWS; i++)
+            {
+                memcopy(get_offset(0, i) + VIDEO_ADDRESS, get_offset(0, i - 1) + VIDEO_ADDRESS, TOTAL_COLS * 2);
+            }
+
+            /*create one blank line at the end*/
+            char *last_line = get_offset(0, TOTAL_ROWS - 1) + VIDEO_ADDRESS;
+            for(i = 0; i < TOTAL_COLS * 2; i++)
+            {
+                last_line[i] = 0;
+            }
+
+            cursorPosition = get_offset(0, TOTAL_ROWS - 1); /*TODO: revisit this.. last line cursor not visible*/
+        }
     }
     set_cursor_position(cursorPosition);
 }
@@ -50,3 +92,13 @@ void set_cursor_position(int offset)
     port_byte_out(REG_SCREEN_CTRL, 15);
     port_byte_out(REG_SCREEN_DATA, offset & 0xff);
 }
+
+int get_current_cursor_row(int cursorPosition) {
+    return cursorPosition / (2 * TOTAL_COLS);
+}
+
+int get_current_cursor_column(int cursorPosition) {
+    return (cursorPosition - (get_current_cursor_row(cursorPosition) * 2 * TOTAL_COLS)) / 2;
+}
+
+int get_offset(int col, int row) { return 2 * (row * TOTAL_COLS + col); }
