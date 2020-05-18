3c0d89beaab901538571910b1df0ded994baf967
dhavalhirdhav <dhaval_faria@hotmail.com>
2020-05-18 20:38:59 +0530

    ata pio driver.

--- drivers/ports.c
+++ drivers/ports.c
@@ -11,8 +11,8 @@ void port_byte_out (unsigned short port, unsigned char data) {
     __asm__("out %%al, %%dx" : : "a" (data), "d" (port));
 }
 
-unsigned char port_word_in (unsigned short port) {
-    unsigned short result;
+uint16_t port_word_in (uint16_t port) {
+    uint16_t result;
     __asm__("in %%dx, %%ax" : "=a" (result) : "d" (port));
     return result;
 }
