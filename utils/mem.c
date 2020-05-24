51fbb2d97ba5e809352293ac730a45ae868b9b36
dhavalhirdhav <dhaval_faria@hotmail.com>
2020-05-24 14:47:00 +0530

    updated file and removed ini file.

--- utils/mem.c
+++ utils/mem.c
@@ -1,9 +1,6 @@
 #include <stdint.h>
 #include "mem.h"
 
-/* This should be computed at link time, but a hardcoded
- * value is fine for now. Remember that our kernel starts
- * at 0x1000 as defined on the Makefile */
 uint32_t free_mem_addr = 0x10000;
 /* Implementation is just a pointer to some free memory which
  * keeps growing */
